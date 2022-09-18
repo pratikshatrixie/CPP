c1f2763d853f292eb16e67917aedd94afc53b810
pratikshatrixie <pratikshatrixie@gmail.com>
2022-09-18 10:33:18 +0530

    floyd

---
+++ floyd1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1;
+    
+    cout << "Enter the number of rows" << endl;
+    cin >> n1;
+    
+    int k = 1;
+    
+    for(int i=1; i<=n1; i++)
+    {
+        for(int j=i; j>0; j--)
+        {
+            cout << k;
+            k++;
+        }
+        
+        cout << endl;
+        
+    }
+
+    return 0;
+}
